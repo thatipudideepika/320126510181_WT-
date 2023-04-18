e6522813238cbd500c5a1d5759642216fbb3d978
thatipudideepika <131149968+thatipudideepika@users.noreply.github.com>
2023-04-18 14:49:32 +0530

    Add files via upload

---
+++ wt/word.js
@@ -0,0 +1,26 @@
+let wordList = [
+  {
+      word: "python",
+      hint: "programming language"
+  },
+  {
+      word: "guitar",
+      hint: "a musical instrument"
+  },
+  {
+      word: "aim",
+      hint: "a purpose or intention"
+  },
+  {
+      word: "earth",
+      hint: "planet of our solar system"
+  },
+  {
+      word: "gold",
+      hint: "a yellow precious metal"
+  },
+  {
+      word: "amazon",
+      hint: "online shopping site"
+  }
+]
